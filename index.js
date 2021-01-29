37c31c802051bb0000ffe8e95ec1684bf450d218
fengyingwei <283705336@qq.com>
2021-01-29 10:02:38 +0800

    test back use my project

--- index.js
+++ index.js
@@ -16,7 +16,7 @@ exports.main_handler = async (event, context, callback) => {
       //})
 
       //3.执行github远端的js文件(因github的raw类型的文件被墙,此方法云函数不推荐)
-      request('https://gitee.com/lxk0301/jd_scripts/raw/master/' + v + '.js', function (error, response, body) {
+      request('https://gitee.com/xiaofeng00/jdScript/raw/master/' + v + '.js', function (error, response, body) {
         eval(response.body)
       })
     }
